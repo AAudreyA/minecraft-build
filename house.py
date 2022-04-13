b1d014ac77cf9224137ca021afe2bbf0617032cf
Audrey Arbuckle <audreyarbuckle@icloud.com>
2022-04-12 21:08:29 -0700

    almost doneee added random house textures yay

--- house.py
+++ house.py
@@ -3,31 +3,46 @@
 from mcpi.minecraft import Minecraft
 from mcpi import block
 import time
+import random
 
 time.sleep(2)
 mc = Minecraft.create()
 x,y,z = mc.player.getTilePos()
 
-#name = input("hello what is ur name? ")
-#mc.postToChat(f"hello {name}") 
 
-name = input("What is your name?")
-num_village = int(input(f"How many houses would you like in your village {name}?"))
+def text(text, toScreen = True, userInput = True):
+    if toScreen == True:
+       mc.postToChat(text)
+    if userInput == True:
+        var = input(text)
+        return var 
+    
+name = text("What is your name?")
+text(f"hi hello {name}fgdhfgnfhftn",userInput = False)
+num_village = int(text(f"How many houses would you like in your village {name}?"))
 # if num_village <
-mc.postToChat(f"Presenting {name}ville!")
+text(f"Presenting {name}ville!", userInput = False)
+text("building village . . . ",toScreen = False, userInput = False)
 
-village = []
+village = [0]
 
 for n in range(num_village):
-    village.extend([7, 7, 10, 7])
-    
-mc.postToChat(village)
+    p = n % 3
+    if p == 0:
+        village.append(10)
+    else:
+        village.append(7)
   
+randomBase = [block.COBBLESTONE.id, block.STONE.id,block.SANDSTONE.id, block.MOSS_STONE.id, block.BRICK_BLOCK.id, block.STONE_BRICK.id, block.NETHER_BRICK.id]
 
 
-def build_house(x,y,z):
+
+def build_house(x,y,z,randomBase):
+   
+    randomBlock = random.choices(randomBase)
+    
     #walls
-    mc.setBlocks(x, y, z+3, x+4, y+4, z+9, block.COBBLESTONE.id)
+    mc.setBlocks(x, y, z+3, x+4, y+4, z+9, randomBlock)
     mc.setBlocks(x+1, y, z+4, x+4-1, y+4, z+8, block.AIR.id)
 
     # floor
@@ -61,7 +76,6 @@ def build_house(x,y,z):
             mc.setBlocks(x+1+i, y+4+i, z+9, x+4-i-1, y+4+i, z+9, block.COBBLESTONE.id, 1)
     return x,y,z 
 
-    
 
 
 #village = [0,7, 10,7]
@@ -70,7 +84,7 @@ def build_house(x,y,z):
 
 for v in village:
      x += v
-     build_house(x,y,z)
+     build_house(x,y,z,randomBase)
 
 #mc.setBlocks(x-100, y, z-100, x+100, y+30, z+100, block.AIR.id)
 #mc.setBlocks(x-100, y, z-100, x+100, y, z+100, block.SAND.id)
